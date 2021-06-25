5b9f4952814e75ae713aadece874b9e8007e725c
甘霖 <thelmg@163.com>
2021-06-25 21:26:09 +0800

    组件

---
+++ pages/live/live.js
@@ -0,0 +1,90 @@
+// pages/live/live.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    content: "",
+    autoSizeConfig: {
+      maxHeight: 300,
+      minHeight: 100
+    },
+    fileList: [],
+  },
+  afterRead(event) {
+    console.log(event);
+    const { file } = event.detail;
+    const _this = this
+    // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+    wx.uploadFile({
+      url: 'https://upload-z1.qiniup.com/', // 仅为示例，非真实的接口地址
+      filePath: file[0].url,
+      name: 'file',
+      formData: { token: 'tZnU5NJbQsOrBVzODD2kzmTRl-e2nwKKCC2Z415o:-kU-fJoiKPDhQm4xX3M4qyWGmFU=:eyJzY29wZSI6Im15bGl2aW5nIiwiZGVhZGxpbmUiOjE2MjM5NDI1MDR9' },
+      success(res) {
+        const data = JSON.parse(res.data)
+        // 上传完成需要更新 fileList
+        console.log(this.data)
+        let fileList = []
+        fileList.push({ ...file, url: "https://qn.soulfree.cn/"+data.key });
+        _this.setData({ fileList });
+      },
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
